d2d4cc3db9b024fd43e2d9940450d52bb9a5d159
Tyler Rogel <d00476330@utahtech.edu>
2024-04-02 16:50:59 -0600

    wolf shows plot now

--- wolf2.py
+++ wolf2.py
@@ -1,12 +1,13 @@
 import random
 import time
+import matplotlib.pyplot as plt
+
 NUM_SHEEP = 9
 def main():
     t1 = time.time()
     win_counter = [0] * NUM_SHEEP
     for n in range(100000):
-        
-        # One round of all sheep dying
+        # One round of all but one sheep dying
         forest = [True] * NUM_SHEEP
         i = 0
         while sum(forest) > 1:
@@ -17,14 +18,20 @@ def main():
                 i -= 1
                 if i < 0:
                     i = NUM_SHEEP -1
-
             else:
                 i += 1
                 if i >= NUM_SHEEP:
                     i = 0
-        
+        # increment the win counter of the last sheep alive by 1
         win_counter[forest.index(True)] += 1
     print("Sheep that lived the longest the most often is: ", win_counter.index(max(win_counter)) )
     t2 = time.time()
     print("Time: ", (t2-t1))
+
+    plt.bar(range(0, NUM_SHEEP), win_counter)
+    plt.xlabel('Sheep')
+    plt.ylabel('Wins')
+    plt.title('Wins by Sheep')
+    plt.show()
+
 main()

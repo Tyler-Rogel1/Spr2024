e8ec4f86ee6d88a0fab615a6d5d91ea5a746d11d
Tyler Rogel <d00476330@utahtech.edu>
2024-04-10 11:45:20 -0600

    find path started

--- graph.py
+++ graph.py
@@ -1,7 +1,9 @@
 # directed and non weighted graph class
 class Graph:
     def __init__(self, vertices):
-        self.neighbors = [[]] * vertices
+        self.neighbors = []
+        for i in range(vertices):
+            self.neighbors.append([])
 
     def addEdge(self, v0, v1):
         self.neighbors[v0].append(v1)
@@ -14,3 +16,18 @@ class Graph:
 
 # breadth first search
     def findPath(self, v0, v1):
+        From = [-1] *len(self.neighbors)
+        Q = []
+        From[v0] = v0
+        Q.append(v0)
+        while Q:
+            c = Q.pop(0)
+            if c == v1:
+                # build path and return it
+                path = []
+                return path.reverse()
+            for n in self.neighbors[c]:
+                if From[n] == -1:
+                    Q.append(n)
+                    From[n] = c
+        return None
